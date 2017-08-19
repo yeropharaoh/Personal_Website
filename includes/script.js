cd1dab0ddcc7e108c3bb072fab9525e3a7899274
yeropharaoh <yeroschutte@gmail.com>
2017-08-20 01:46:24 +0200

    final changes to index

--- includes/script.js
+++ includes/script.js
@@ -1,16 +1,46 @@
-const Typed = require('typed.js');
-const express = require('express')
-const app = express()
+// const Typed = require('typed.js');
+// const express = require('express')
+// const app = express()
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`)
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(`${__dirname}/index.html`)
+// })
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+// app.listen(3000, () => {
+//   console.log('Example app listening on port 3000!')
+// })
 
-app.use(express.static(`${__dirname}/`))
+// app.use(express.static(`${__dirname}/`))
+
+
+// $("#typed").typed({
+//     strings: ["<h1>Hello,^250 I'm ^50<span>Wolf</span> </h1>"],
+//     typeSpeed: -15,
+//     startDelay: 0,
+//     showCursor: true,
+//     showCursor2: false,
+//     contentType: 'html',
+//     callback: function() {
+//       $( ".typed-cursor" ).fadeOut( "100", function() {});
+//       $( ".typed-cursor2" ).delay(50).fadeIn( "100", function() {});
+//     }
+//   });
+
+//     $("#typed2").typed({
+//     strings: ["<p>I am a ^10Shader ^10&amp; ^10VFX ^10Technical ^10Artist</p>"],
+//     typeSpeed: -15,
+//     startDelay: 1500,
+//     showCursor: false,
+//     showCursor2: true,
+//     contentType: 'html',
+//     callback: function() {
+//       $( ".typed-cursor2" ).delay(0).fadeOut( "500", function() {});
+//       $( ".about-btn" ).delay(1000).fadeIn( "500", function() {});
+//     // $( ".about-btn" ).delay(1000).show(function() {});
+//     /*only fade in above certain screensize */
+//     }
+//   });
+
 
 
 
