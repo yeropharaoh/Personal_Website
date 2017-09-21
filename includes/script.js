6bc6e58345e4c37804809b598021a7bbc0eac7bb
yeropharaoh <yeroschutte@gmail.com>
2017-09-21 13:42:39 +0200

    styling

--- includes/script.js
+++ includes/script.js
@@ -1,68 +1,12 @@
-// const Typed = require('typed.js');
-// const express = require('express')
-// const app = express()
-
-// app.get('/', (req, res) => {
-//   res.sendFile(`${__dirname}/index.html`)
-// })
-
-// app.listen(3000, () => {
-//   console.log('Example app listening on port 3000!')
-// })
-
-// app.use(express.static(`${__dirname}/`))
-
-
-// $("#typed").typed({
-//     strings: ["<h1>Hello,^250 I'm ^50<span>Wolf</span> </h1>"],
-//     typeSpeed: -15,
-//     startDelay: 0,
-//     showCursor: true,
-//     showCursor2: false,
-//     contentType: 'html',
-//     callback: function() {
-//       $( ".typed-cursor" ).fadeOut( "100", function() {});
-//       $( ".typed-cursor2" ).delay(50).fadeIn( "100", function() {});
-//     }
-//   });
-
-//     $("#typed2").typed({
-//     strings: ["<p>I am a ^10Shader ^10&amp; ^10VFX ^10Technical ^10Artist</p>"],
-//     typeSpeed: -15,
-//     startDelay: 1500,
-//     showCursor: false,
-//     showCursor2: true,
-//     contentType: 'html',
-//     callback: function() {
-//       $( ".typed-cursor2" ).delay(0).fadeOut( "500", function() {});
-//       $( ".about-btn" ).delay(1000).fadeIn( "500", function() {});
-//     // $( ".about-btn" ).delay(1000).show(function() {});
-//     /*only fade in above certain screensize */
-//     }
-//   });
-
-
-
-
-// var typed = new Typed('.element', {
-//   strings: ["First sentence.", "Second sentence."],
-//   typeSpeed: 30
-// });
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var typed = new Typed('#typed', {
-//     stringsElement: '#typed-strings',
-//     typeSpeed: 20,
-//     backSpeed: 20,
-//     startDelay: 1000,
-//     loop: false,
-//     loopCount: Infinity,
-//   });
-// });
-
-// var typed = new Typed('.element', {
-//   strings: ["First sentence.", "Second sentence."],
-//   typeSpeed: 30
-// });
-
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    strings: ['Hi!', 'I am: Yero &copy;', 'I am: <strong>an aspiring developer</strong>', 'I am: <strong>looking for opportunities!</strong>'],
+    // stringsElement: '#typed-strings',
+    typeSpeed: 20,
+    backSpeed: 20,
+    startDelay: 1000,
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity,
+  });
+});
